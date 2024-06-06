23f1aeeb45eaec9fb2277959d07c9f63dcdcff37
Jad El Nader <Jadelnader99@gmail.com>
2024-06-06 18:49:43 +0200

    add message and chat

--- chat/views.py
+++ chat/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
+from .models import Message, Chat
 
 # Create your views here.
 def index(request):
+    if request.method =='POST':
+      print("Received data" + request.POST['textmessage'])
+      myChat = Chat.objects.get(id=2)
+      Message.objects.create(text = request.POST['textmessage'], chat=myChat, author=request.user, receiver=request.user)
     return render(request, 'chat/index.html', {'username':'Jad'})
