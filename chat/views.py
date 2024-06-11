0986d0dc1fb441461abfeeb76486e87528920b8f
Jad El Nader <Jadelnader99@gmail.com>
2024-06-11 19:27:03 +0200

    load current message from bckend

--- chat/views.py
+++ chat/views.py
@@ -1,9 +1,11 @@
-from django.http import HttpResponseRedirect
+from django.http import HttpResponseRedirect, JsonResponse
 from django.shortcuts import render
 from .models import Message, Chat
 from django.contrib.auth import authenticate, login
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
+from django.core import serializers
+import json
 
 # Create your views here.
 @login_required(login_url='/login/')
@@ -11,7 +13,13 @@ def index(request):
     if request.method =='POST':
       print("Received data" + request.POST['textmessage'])
       myChat = Chat.objects.get(id=2)
-      Message.objects.create(text = request.POST['textmessage'], chat=myChat, author=request.user, receiver=request.user)
+      new_message =  Message.objects.create(text = request.POST['textmessage'], chat=myChat, author=request.user, receiver=request.user)
+      serialized_obj = serializers.serialize('json', [new_message,])
+      message_data = json.loads(serialized_obj)[0]
+       # Modify the author field to include the username
+      message_data['fields']['author'] = request.user.username  
+      message_data['fields']['created_at'] = new_message.created_at.strftime('%B %d, %Y')
+      return JsonResponse(message_data, safe=False)
     chatMessages = Message.objects.filter(chat__id=2)
     return render(request, 'chat/index.html', {'messages':chatMessages})
 
